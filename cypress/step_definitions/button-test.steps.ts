d10c7af968313c4b2adf0ef9879a638d79d9a64e
lavioli-work <lavie.ruan@semanticbits.com>
2021-05-19 15:53:15 -0500

    change text

--- cypress/step_definitions/button-test.steps.ts
+++ cypress/step_definitions/button-test.steps.ts
@@ -4,5 +4,5 @@ Then('User clicks on the button and verifies the changed title', () => {
   cy.visit('https://cypress-github-actions.web.app/');
   cy.getByCy('title').should('contain.text', 'Hello');
   cy.getByCy('click-me-button').click();
-  cy.getByCy('title').should('contain.text', 'Cypress Github Actions Demo!');
+  cy.getByCy('title').should('contain.text', 'Cypress Github Actions Demo!!');
 });
